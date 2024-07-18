dd77f9020a381d787c8a499cf111a9ba4a5e8cfc
Shadrack Essien <shadrack6960@gmail.com>
2024-07-18 00:30:44 +0000

    Added jps to the categories component

--- frontend/app/components/Categories.tsx
+++ frontend/app/components/Categories.tsx
@@ -1,7 +1,97 @@
+import Image from "next/image";
+
 function Categories() {
   return (
-    <div>
-      <div>Categories</div>
+    <div className="pt-3 cursor-pointer pb-6 flex items-center space-x-12">
+      <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:border-gray-200 hover:opacity-100">
+        <Image
+          src="/amazing_views.jpg"
+          alt="Amazing views pic"
+          width={20}
+          height={20}
+        />
+        <span className="text-xs">Amazing Views</span>
+      </div>
+
+      <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:border-gray-200 hover:opacity-100">
+        <Image
+          src="/cabins.jpg"
+          alt="Amazing views pic"
+          width={20}
+          height={20}
+        />
+        <span className="text-xs">Cabins</span>
+      </div>
+
+      <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:border-gray-200 hover:opacity-100">
+        <Image
+          src="/castles.jpg"
+          alt="Amazing views pic"
+          width={20}
+          height={20}
+        />
+        <span className="text-xs">Castles</span>
+      </div>
+
+      <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:border-gray-200 hover:opacity-100">
+        <Image
+          src="/designs.jpg"
+          alt="Amazing views pic"
+          width={20}
+          height={20}
+        />
+        <span className="text-xs">Designs</span>
+      </div>
+
+      <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:border-gray-200 hover:opacity-100">
+        <Image
+          src="/earth_homes.jpg"
+          alt="Amazing views pic"
+          width={20}
+          height={20}
+        />
+        <span className="text-xs">Earth Homes</span>
+      </div>
+
+      <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:border-gray-200 hover:opacity-100">
+        <Image
+          src="/farms.jpg"
+          alt="Amazing views pic"
+          width={20}
+          height={20}
+        />
+        <span className="text-xs">Farms</span>
+      </div>
+
+      <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:border-gray-200 hover:opacity-100">
+        <Image
+          src="/mansions.jpg"
+          alt="Amazing views pic"
+          width={20}
+          height={20}
+        />
+        <span className="text-xs">Mansions</span>
+      </div>
+
+      <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:border-gray-200 hover:opacity-100">
+        <Image
+          src="/sheperds_hut.jpg"
+          alt="Amazing views pic"
+          width={20}
+          height={20}
+        />
+        <span className="text-xs">Sheperds Hut</span>
+      </div>
+
+      <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:border-gray-200 hover:opacity-100">
+        <Image
+          src="/rooms.jpg"
+          alt="Amazing views pic"
+          width={20}
+          height={20}
+        />
+        <span className="text-xs">Rooms</span>
+      </div>
     </div>
   );
 }
